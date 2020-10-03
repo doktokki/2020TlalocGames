e90d13af7d3b0fe46913dedd70269832c2be88f1
doktokki <72271221+doktokki@users.noreply.github.com>
2020-10-03 22:57:48 +0900

    Update js-image-slider.js

--- dist/js/js-image-slider.js
+++ dist/js/js-image-slider.js
@@ -9,7 +9,7 @@ var sliderOptions=
 	slices: 12,
 	boxes: 8,
 	hoverPause: 1,
-	autoAdvance: true,
+	autoAdvance: false,
 	captionOpacity: 0.3,
 	captionEffect: "fade",
 	thumbnailsWrapperId: "thumbs",
